4c6b629cd7df2ac369b7ec2d010c2014fae91aef
jaydenridley4 <79724748+jaydenridley4@users.noreply.github.com>
2022-09-11 08:57:19 +1000

    Delete liteIE directory

--- liteIE/Form1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Form1
-    Private Sub GO_Click(sender As Object, e As EventArgs) Handles GO.Click
-        WebBrowser1.Navigate(TextBox1.Text)
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        WebBrowser1.Navigate("http://www.google.com/search?q=" & TextBox1.Text)
-    End Sub
-End Class
